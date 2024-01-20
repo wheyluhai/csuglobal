12dbfdca00c129ca00f1ccb55b18d8494dd9401e
AYU <wheyluhai@gmail.com>
2024-01-19 20:47:23 -0500

    Slight cleanup for part 1.

--- CSC500-1/Module_5/critical_thinking_part1.py
+++ CSC500-1/Module_5/critical_thinking_part1.py
@@ -1,8 +1,3 @@
-# Part 1:
-# Write a program that uses nested loops to collect data and calculate the average rainfall over a period of years. The program should first ask for the number of years. The outer loop will iterate once for each year. The inner loop will iterate twelve times, once for each month. Each iteration of the inner loop will ask the user for the inches of rainfall for that month. After all iterations, the program should display the number of months, the total inches of rainfall, and the average rainfall per month for the entire period.
-
-import random
-
 # Get number of years
 def get_years():
     years = input("Please enter the number of years of data you will provide and press enter: ")
